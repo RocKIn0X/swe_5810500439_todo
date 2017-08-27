c46b56cc8d3f016a1fd0bbca765591796602055e
Dantai <dantai39@gmail.com>
2017-08-27 21:09:13 +0700

    create superuser

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
+from __future__ import unicode_literals
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_at = models.DateTimeField(default=datetime.now, blank=True)
