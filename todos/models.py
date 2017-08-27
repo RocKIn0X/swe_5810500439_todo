392328a8c9658beded4595495c1dceed1666876a
Dantai <dantai39@gmail.com>
2017-08-27 21:39:49 +0700

    add index.html

--- todos/models.py
+++ todos/models.py
@@ -7,3 +7,5 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_at = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.title
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
+
 def index(request):
-    return HttpResponse('Hello world')
+    todos = Todo.objects.all()[:10]
+
+    context = {
+        'todos':todos
+    }
+
+    return render(request, 'index.html', context)
